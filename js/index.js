e3f773348127174f46c5579e80b4399034c00fb6
Alex - Elevate <alex-elevate@Alexs-MacBook-Air.local>
2017-02-06 13:45:16 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+// ------ floated "We Are" at top of page -------
+const weArr = [
+  'makers',
+  'doers',
+  'thinkers',
+  'crafters',
+  'shakers',
+  'explorers',
+  'seekers',
+  'pioneers',
+  'pathfinders',
+  'shapers',
+  'dreamers'
+]
+
+const dynamic = $('.dynamic')
+
+let i = 0
+let len = weArr.length
+
+let weArrLoop = () => {
+  dynamic.empty()  // empties div of children
+  let item = weArr[i]
+  i++
+  if (i === len) { // resets counter to 0 once i = length of array
+    i = 0
+  }
+
+  let block = `
+  <p>&nbsp;${item}</p>
+  `
+  dynamic.append(block)
+
+  return i
+}
+
+setInterval(weArrLoop, 2000)
